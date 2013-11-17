147852cb945475a240045d6fe78fc9823928c2e2
Yassin Gamal <yassingamal@gmail.com>
2013-11-17 16:42:59 +0200

    #New project

---
+++ PaperV/PaperV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaperV
+//
+//  Created by Yassin Gamal on 11/17/13.
+//  Copyright (c) 2013 Triangles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PaperV/PaperV/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PaperV
+//
+//  Created by Yassin Gamal on 11/17/13.
+//  Copyright (c) 2013 Triangles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
